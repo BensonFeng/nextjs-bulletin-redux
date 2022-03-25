08fdb6807e7f7cf0e0d655afd82536ace630bce4
BensonFeng <bensonfeng@outlook.com>
2022-03-25 14:40:28 -0400

    Display list of posts

--- features/posts/postsSlice.js
+++ features/posts/postsSlice.js
@@ -1,6 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = [];
+const initialState = [
+  {
+    id: "1",
+    title: "Redux toolkit",
+    content: "A modern tool to use redux.",
+  },
+  { id: "2", title: "NVDIA graphics card", content: "Huge GPU price drop." },
+];
 
 const postsSlice = createSlice({
   name: "posts",
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 import Head from "next/head";
 import Image from "next/image";
+import PostList from "../features/posts/PostList";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <main className="App"></main>;
+  return (
+    <main className="App">
+      <PostList />
+    </main>
+  );
 }
