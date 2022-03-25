e14cbbf547152333827689e6b75b3d7252d6db4c
BensonFeng <bensonfeng@outlook.com>
2022-03-25 15:17:10 -0400

    create form

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
+import AddPostForm from "../features/posts/AddPostForm";
 import PostList from "../features/posts/PostList";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <main className="App">
+      <AddPostForm />
       <PostList />
     </main>
   );
