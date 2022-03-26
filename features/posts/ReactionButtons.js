9e0f0fdd4eb5489c686b5610c7a088d785e0a48c
BensonFeng <bensonfeng@outlook.com>
2022-03-26 17:56:17 -0400

    ReactionButtons into PostsList

--- features/posts/ReactionButtons.js
+++ features/posts/ReactionButtons.js
@@ -2,7 +2,7 @@ import { useDispatch } from "react-redux";
 import { reactionAdded } from "./postsSlice";
 
 const reactionEmoji = {
-  thumbsUp: "👍",
+  thumbsup: "👍",
   wow: "😮",
   heart: "❤️",
   rocket: "🚀",
