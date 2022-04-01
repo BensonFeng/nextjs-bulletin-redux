4b72ad870a453f3db0cdfd29306db3a3c0b02f7b
BensonFeng <bensonfeng@outlook.com>
2022-04-01 18:41:38 -0400

    display author

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
 import "../styles/globals.css";
 import { store } from "../app/store";
 import { Provider } from "react-redux";
+import { fetchUsers } from "../features/users/usersSlice";
 
+store.dispatch(fetchUsers());
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
