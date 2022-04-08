5700813bdf7179fdd3b4fcf7e34113ffc99cf2c3
BensonFeng <bensonfeng@outlook.com>
2022-04-08 19:29:50 -0400

    content stay after refresh

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import "../styles/globals.css";
 import { store } from "../app/store";
 import { Provider } from "react-redux";
+import { fetchPosts } from "../features/posts/postsSlice";
 import { fetchUsers } from "../features/users/usersSlice";
 import Layout from "../components/Layout";
 
+store.dispatch(fetchPosts());
 store.dispatch(fetchUsers());
+
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
