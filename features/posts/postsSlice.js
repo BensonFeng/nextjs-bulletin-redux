61970e6c2615671c0e63ffc4634395fe21447c97
BensonFeng <bensonfeng@outlook.com>
2022-04-15 19:10:50 -0400

    entity adapter generated selectors

--- features/posts/postsSlice.js
+++ features/posts/postsSlice.js
@@ -172,13 +172,21 @@ const postsSlice = createSlice({
   },
 });
 
-export const selectAllPosts = (state) => state.posts.posts;
+//getSelectors creates these selectors and we rename them with aliases using destructuring
+export const {
+  selectAll: selectAllPosts,
+  selectById: selectPostsById,
+  selectIds: selectPostIds,
+  //Pass in a selector that returns the posts slice of state
+} = postsAdapter.getSelectors((state) => state.posts);
+
+// export const selectAllPosts = (state) => state.posts.posts;
 export const getPostsStatus = (state) => state.posts.status;
 export const getPostsError = (state) => state.posts.error;
 export const getCount = (state) => state.posts.count;
 
-export const selectPostById = (state, postId) =>
-  state.posts.posts.find((post) => post.id === postId);
+// export const selectPostById = (state, postId) =>
+//   state.posts.posts.find((post) => post.id === postId);
 
 export const selectPostByUser = createSelector(
   [selectAllPosts, (state, userId) => userId],
